9d496efe871ea795298594c2041c0ade02c0f50b
Harshal P. Jadhav <150316903+Harshalj2003@users.noreply.github.com>
2026-05-25 11:34:26 +0530

    Add: is-anagram - submission-17

---
+++ Data Structures & Algorithms/is-anagram/submission-17.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) - 1);
+        }
+        
+        for(int value : map.values()) if(value != 0) return false;
+        return true;
+
+        // int[] arr = new int[26];
+
+        // for(int i = 0; i < s.length(); i++){
+        //     arr[s.charAt(i) - 'a']++;
+        //     arr[t.charAt(i) - 'a']--;
+        // }
+
+        // for(int i : arr) if(i != 0) return false;
+        // return true;
+    }
+}
