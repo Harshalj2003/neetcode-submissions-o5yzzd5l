ca30d12262c2aa840fea56e6e66f69c1fb1f5f17
Harshal P. Jadhav <150316903+Harshalj2003@users.noreply.github.com>
2026-05-26 09:02:09 +0530

    Add: two-integer-sum - submission-19

---
+++ Data Structures & Algorithms/two-integer-sum/submission-19.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        if(nums.length < 2) return new int[]{-1};
+
+        Map<Integer, Integer> sheet = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            if(sheet.get(nums[i]) == null){
+                sheet.put(target - nums[i], i);
+            }else{
+                return new int[]{sheet.get(nums[i]), i};
+            }
+        }return null;
+    }
+}
