e01a3e78b05d0fd42eb46675ae569fbdb89c9bb1
Harshal P. Jadhav <150316903+Harshalj2003@users.noreply.github.com>
2026-06-23 17:29:51 +0530

    Add: merge-two-sorted-linked-lists - submission-3

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-3.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+        while (list1 != null || list2 != null) {
+            int val1 = list1 != null ? list1.val : Integer.MAX_VALUE;
+            int val2 = list2 != null ? list2.val : Integer.MAX_VALUE;
+            if (val1 < val2) {
+                cur.next = list1;
+                cur = list1;
+                list1 = list1.next;
+            } else {
+                cur.next = list2;
+                cur = list2;
+                list2 = list2.next;
+            }
+        }
+        return dummy.next;
+    }
+}
